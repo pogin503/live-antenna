e25d68a458b76ee881a499b428fb71571c17c4d2
yamahare <jidai.norio@gmail.com>
2018-12-10 00:40:53 +0900

    capistranoのdeploy.rbを修正。branchがmasterを明示する必要なし

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,6 @@ set :repo_url, "git@github.com:yamahare/live-antenna.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-set :branch, master
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/var/www/live-antenna"
