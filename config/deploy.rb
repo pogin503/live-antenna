1b0545f2b9a2b6edd97e0ea01c6c817784c9bf25
yamahare <jidai.norio@gmail.com>
2018-12-10 00:37:08 +0900

    capistranoのdeploy.rbとdeploy/production.rb設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,15 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "live-antenna"
+set :repo_url, "git@github.com:yamahare/live-antenna.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, master
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/var/www/live-antenna"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
