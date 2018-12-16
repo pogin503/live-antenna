b2ad8de983ac0cbf175f5ac5f6863ed884ef4101
yamahare <jidai.norio@gmail.com>
2018-12-16 17:34:52 +0900

    [add]deploy.rbにmaster.keyのlinkを追加

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :deploy_to, "/var/www/live-antenna"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+append :linked_files, "config/master.key"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets"
